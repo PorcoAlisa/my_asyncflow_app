bccdea35a620a1aed1b0cd4c528df37b5e4e6308
porco <porcorossj@gmail.com>
2025-12-18 09:09:56 +0000

    hold_task

--- flowsvr/src/ctrl/hold_task.cc
+++ flowsvr/src/ctrl/hold_task.cc
@@ -3,12 +3,37 @@
 using namespace async_flow::frmwork;
 
 
-Task<Status> HoldTaskHandler::HandleInput(std::shared_ptr<api::HoldTasksReq>& reqBody) {
+const int32_t HoldTasksHandler::MAX_TASK_LIST_LIMIT = 1000;
+const int32_t HoldTasksHandler::DEFAULT_TASK_LIST_LIMIT = 1000;
+
+Task<Status> HoldTasksHandler::HandleInput(std::shared_ptr<api::HoldTasksReq>& reqBody) {
     if (reqBody->task_type().empty()) {
-        
+        LOG_ERROR << "task type is empty";
+        co_return InputInvalid;
     }
+    co_return Status::OK;
 }
 
-Task<Status> HoldTaskHandler::HandleProcess(std::shared_ptr<api::HoldTasksReq>& reqBody, api::HoldTasksRsp& rspBody) {
+Task<Status> HoldTasksHandler::HandleProcess(std::shared_ptr<api::HoldTasksReq>& reqBody, api::HoldTasksRsp& rspBody) {
+    TaskDao taskDao;
+    SchedulePosDao posDao;
+    TSchedulePos schPos;
 
+    int limit = reqBody->limit();
+    if (limit > MAX_TASK_LIST_LIMIT) {
+        limit = MAX_TASK_LIST_LIMIT;
+    }
+
+    if (limit == 0) {
+        limit = DEFAULT_TASK_LIST_LIMIT;
+    }
+    std::string taskType = reqBody->task_type();
+    std::string taskTableName = GetTaskTableName(taskType);
+
+    Status status = co_await posDao.GetAsync(taskTableName, schPos);
+    if (!status.ok()) {
+        co_return Status::FAIL;
+    }
+    std::string beginSchPos = std::to_string(schPos.getValueOfScheduleBeginPos());
+    co_return Status::OK;
 }
--- flowsvr/src/ctrl/hold_task.h
+++ flowsvr/src/ctrl/hold_task.h
@@ -7,10 +7,13 @@
 namespace async_flow {
 namespace flowsvr {
 
-class HoldTaskHandler : public Handler<api::HoldTasksReq, api::HoldTasksRsp> {
+class HoldTasksHandler : public Handler<api::HoldTasksReq, api::HoldTasksRsp> {
 public:
     virtual Task<frmwork::Status> HandleInput(std::shared_ptr<api::HoldTasksReq>& reqBody) override;
     virtual Task<frmwork::Status> HandleProcess(std::shared_ptr<api::HoldTasksReq>& reqBody, api::HoldTasksRsp& rspBody) override;
+    
+    static const int32_t MAX_TASK_LIST_LIMIT;
+    static const int32_t DEFAULT_TASK_LIST_LIMIT;
 };
 
 } // flowsvr
