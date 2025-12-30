d88952f5939cd4f21fedf99323e13ec4ae32ae71
Porco <porcorossj@gmail.com>
2025-12-30 03:34:15 +0000

    新增Hold函数

--- worker/src/sdk/schedule.cc
+++ worker/src/sdk/schedule.cc
@@ -15,15 +15,14 @@ drogon::Task<void> TaskMgr::LoadCfgLoop() {
     }
     while (true) {
         api::GetTaskScheduleCfgListReq req;
-        auto resultPair = co_await taskRpc->GetTaskScheduleCfgList(req);
-        if (!resultPair.second.ok()) {
+        auto [rsp, status] = co_await taskRpc->GetTaskScheduleCfgList(req);
+        if (!status.ok()) {
             // 目前错误返回的都是FAIL，error_code都是一样的，这里没啥意义
-            LOG_ERROR << "GetTaskScheduleCfgList err: " << resultPair.second.error_code();
+            LOG_ERROR << "GetTaskScheduleCfgList err: " << status.error_code();
             co_await drogon::sleepCoro(trantor::EventLoop::getEventLoopOfCurrentThread(), 5.0);
             continue;
         }
         std::map<std::string, api::TaskScheduleCfg> newMap;
-        api::GetTaskScheduleCfgListRsp& rsp = resultPair.first;
         LOG_INFO << "schedule cfg list size: " << rsp.task_schedule_cfg_size();
         for (int i = 0; i < rsp.task_schedule_cfg_size(); ++i) {
             newMap[rsp.task_schedule_cfg(i).task_type()] = rsp.task_schedule_cfg(i);
@@ -47,6 +46,34 @@ void TaskMgr::Init() {
             return self->LoadCfgLoop();
         });
     });
+}
+
+drogon::Task<std::vector<TaskPtr>> TaskMgr::Hold(const api::TaskScheduleCfg& cfg) {
+    auto* taskRpc = drogon::app().getPlugin<TaskRpc>();
+    if (!taskRpc) {
+        LOG_ERROR << "TaskRpc plugin not found! Did you add it to config.json?";
+        co_return {};
+    }
+
+    api::HoldTasksReq req;
+    req.set_task_type(taskType_);
+    req.set_limit(cfg.schedule_limit());
+
+    auto [rsp, status] = co_await taskRpc->HoldTasks(req);
+    if (!status.ok()) {
+        LOG_ERROR << "hold task failed.";
+        co_return {};
+    }
+    if (rsp.task_list_size() == 0) {
+        LOG_INFO << "hold task is empty.";
+        co_return {};
+    }
+    std::vector<TaskPtr> tasks;
+    tasks.reserve(rsp.task_list_size());
+    for (size_t i = 0; i < rsp.task_list_size(); ++i) {
+        tasks.emplace_back(factory_(std::move(rsp.task_list(i)), taskSvrHost_));
+    }
+    co_return tasks;
 }
 
 }
