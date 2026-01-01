cf2504366f0bd11e9feff855b9308a7a1037a00d
Porco <porcorossj@gmail.com>
2026-01-01 10:25:59 +0000

    修复config的建议

--- worker/src/main.cc
+++ worker/src/main.cc
@@ -12,8 +12,7 @@ int main() {
     std::string taskType = "lark";
     std::string taskHost = "http:127.0.0.1:39002";
 
-    auto config = drogon::app().getCustomConfig();
-    if (config.isMember("app_settings")) {
+    if (auto config = drogon::app().getCustomConfig(); config.isMember("app_settings")) {
         taskType = config["app_settings"].get("type", taskType).asString();
         taskHost = config["app_settings"].get("host", taskHost).asString();
     }
