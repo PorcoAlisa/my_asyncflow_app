2a6d4014bc15f3bf045c901b820e04255988eba9
Porco <porcorossj@gmail.com>
2026-01-04 08:16:15 +0000

    修改日志级别

--- flowsvr/src/main.cc
+++ flowsvr/src/main.cc
@@ -16,6 +16,8 @@ int main() {
     RegisterHandler<HoldTasksHandler>(drogon::app(), "/hold_tasks");
     RegisterHandler<SetTaskHandler>(drogon::app(), "/set_task");
     RegisterHandler<GetTaskScheduleCfgListHandler>(drogon::app(), "/get_task_schedule_cfg_list");
+    // 不知道为什么config.json里面设置的ERROR级别不生效，只能在这里手动改成ERROR级别
+    trantor::Logger::setLogLevel(trantor::Logger::kError);
     drogon::app().loadConfigFile("./flowsvr/config/config.json").run();
     return 0;
 }
