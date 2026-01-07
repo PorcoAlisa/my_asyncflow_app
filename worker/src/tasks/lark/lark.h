ec1fbad2449616aa7c7a7679dedc2a27ad955204
Porco <Porco.my@qqc.om>
2026-01-07 15:18:26 +0800

    Lark中修改任务状态为成功

--- worker/src/tasks/lark/lark.h
+++ worker/src/tasks/lark/lark.h
@@ -7,7 +7,10 @@ namespace async_flow::worker {
 class Lark : public TaskBase {
 public:
     Lark(api::TaskData data, std::string_view host) : TaskBase(std::move(data), host) {}
-
+    drogon::Task<frmwork::Status> HandleProcess() override {
+        taskData_.set_status(frmwork::TASK_SUCC);
+        co_return frmwork::Status::OK;
+    }
     static TaskPtr CreateLark(api::TaskData data, std::string_view host);
 };
 
